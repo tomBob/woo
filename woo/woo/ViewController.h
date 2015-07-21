0414133de69e80188c80ff0dddbdafa35423df2d
tarena226 <tarena226@Taotao.local>
2015-07-21 17:23:31 +0800

    dkahksdfh

---
+++ woo/woo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  woo
+//
+//  Created by tarena226 on 15/7/21.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ woo/woo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  woo
+//
+//  Created by tarena226 on 15/7/21.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
